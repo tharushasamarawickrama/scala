a8fcc7cc403c0fae40ee2754050265e70744203e
tharushasamarawickrama <tharushathathsaraviduranga@gmail.com>
2024-06-17 14:07:16 +0530

    scala functions

---
+++ labsheet/labsheet01.scala
@@ -0,0 +1,10 @@
+//Question01
+def areaofdisk(r: Double)=math.Pi*r*r;
+//Question02
+def convertTemperature(c:Double)=c*1.8000+32.00;
+//Question03
+def volumeofsphere(r: Double)=(4*math.Pi*r*r*r)/3;
+//Question04
+def cost()=(24.95*0.6) + 3+10*0.75;
+//question05
+def  spendtime()=2*8+3*7+2*8;
