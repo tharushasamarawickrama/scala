fc5643f96d54ee0463f6705d1918b31d4f9fb161
tharushasamarawickrama <tharushathathsaraviduranga@gmail.com>
2024-06-17 14:23:47 +0530

    cleare logical error

--- labsheet/labsheet01.scala
+++ labsheet/labsheet01.scala
@@ -5,6 +5,6 @@ def convertTemperature(c:Double)=c*1.8000+32.00;
 //Question03
 def volumeofsphere(r: Double)=(4*math.Pi*r*r*r)/3;
 //Question04
-def cost()=(24.95*0.6) + 3+10*0.75;
+def cost()=(24.95*0.6*60) + 3*50+10*0.75;
 //question05
 def  spendtime()=2*8+3*7+2*8;
