35b09a77f4ee16c1200ec2b736bd902646e0e2d6
tharushasamarawickrama <tharushathathsaraviduranga@gmail.com>
2024-06-24 22:43:52 +0530

    Q4 complete

---
+++ labsheet/labsheet02_Q4.scala
@@ -0,0 +1,28 @@
+object question04{
+    def main(args: Array[String]): Unit ={
+        println(profit(15));
+        println(profit(20));
+        println(profit(25));
+        println(profit(30));
+
+    }
+}
+def noOfAttendees(ticket_price: Double): Double={
+        120 + (15 - ticket_price) * 4 // /5 *20
+    }
+def cost(ticket_price: Double):Double={
+    var noOfshow= (noOfAttendees(ticket_price)/120)
+    if(noOfshow <= 1){
+        noOfshow=1
+    }else{
+        noOfshow=2
+    }
+    
+       noOfshow*500+noOfAttendees(ticket_price)*3 
+    }
+def income(ticket_price:Double):Double={
+    noOfAttendees(ticket_price)*ticket_price
+}
+def profit(ticket_price:Double):Double={
+    income(ticket_price)-cost(ticket_price)
+}
