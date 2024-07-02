976379249c1a2cbd0d99a03c557fee345ce3a41e
tharushasamarawickrama <tharushathathsaraviduranga@gmail.com>
2024-07-02 07:23:35 +0530

    eddit Q1

--- labsheet03/Q1.scala
+++ labsheet03/Q1.scala
@@ -1,13 +1,14 @@
-object StringReverser {
+object StringReverser extends App {
   def reverseString(s: String): String = s match {
     case "" => "" // Base case: if the string is empty, return an empty string
     case _ => reverseString(s.tail) + s.head // Recursive case: reverse the tail and append the head
   }
-  
-  def main(args: Array[String]): Unit = {
-    // Example usage:
-  val inputString = "tharusha thathrasa" 
+
+  // Get user input
+  println("Enter a string to reverse:")
+  val inputString = scala.io.StdIn.readLine()
+
+  // Output the original and reversed string
   println(s"Original string: $inputString")
   println(s"Reversed string: ${reverseString(inputString)}")
-  }
 }
